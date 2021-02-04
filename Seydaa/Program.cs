64aec47701099b8d63d15d2395b46c358f77ea25
seydax <seydaozturk96@gmail.com>
2021-02-04 20:57:04 +0300

    Seyda

---
+++ Seydaa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seydaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
